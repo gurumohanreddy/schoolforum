caee05729c63acb1edfc709df1fd448a84900a8f
gurumohanreddy <gurumohan449@gmail.com>
2015-12-28 14:58:35 +0530

    restricted access to other pages without login

--- src/app/components/menu/menu.controller.js
+++ src/app/components/menu/menu.controller.js
@@ -1,19 +1,29 @@
 
 myApp.controller('MenuController',['menuservice','$state',function(menuservice,$state){
         var menu = this;
-
         Parse.initialize("OXJqhw8TXb4dgsQXw3uTo3IxYGdL1pj0qjV0g0vW","B6fAQ3HcASbpb4hKrAVoVgMy5p2U5CGe3CXLGJPg");
+        var currentUser = Parse.User.current();
 
-        function populatemenu(){
-            menu.menuitems = menuservice.getdata();
+        if (currentUser) {
+
+
+          function populatemenu(){
+              menu.menuitems = menuservice.getdata();
+          }
+
+          menu.logout = function(){
+            Parse.User.logOut();
+          }
+
+          menu.menuitems = [];
+          populatemenu();
+
+
+        } else {
+            $state.go('home');
         }
 
-        menu.logout = function(){
-          Parse.User.logOut();
-        }
 
-        menu.menuitems = [];
-        populatemenu();
 
 
 }]);
