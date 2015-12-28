5d644459005e64a5ab44dacc18b736df83f0bb3b
gurumohanreddy <gurumohan449@gmail.com>
2015-12-28 14:20:49 +0530

    basic logout is done

--- src/app/components/menu/menu.controller.js
+++ src/app/components/menu/menu.controller.js
@@ -1,11 +1,17 @@
-myApp.controller('MenuController',['menuservice',function(menuservice){
+
+myApp.controller('MenuController',['menuservice','$state',function(menuservice,$state){
+        var menu = this;
+
+        Parse.initialize("OXJqhw8TXb4dgsQXw3uTo3IxYGdL1pj0qjV0g0vW","B6fAQ3HcASbpb4hKrAVoVgMy5p2U5CGe3CXLGJPg");
 
         function populatemenu(){
             menu.menuitems = menuservice.getdata();
         }
 
+        menu.logout = function(){
+          Parse.User.logOut();
+        }
 
-        var menu = this;
         menu.menuitems = [];
         populatemenu();
 
