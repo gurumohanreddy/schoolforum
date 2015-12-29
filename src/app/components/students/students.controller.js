d5ff7f17d4fb871d44e7a03370ce16ce9b88b198
gurumohanreddy <gurumohan449@gmail.com>
2015-12-29 13:13:42 +0530

    POST in student page

--- src/app/components/students/students.controller.js
+++ src/app/components/students/students.controller.js
@@ -1,8 +1,9 @@
-myApp.controller('StudentsController',['$state',function($state){
+myApp.controller('StudentsController',['$state','$http',function($state,$http){
 
         var students = this;
         Parse.initialize("OXJqhw8TXb4dgsQXw3uTo3IxYGdL1pj0qjV0g0vW","B6fAQ3HcASbpb4hKrAVoVgMy5p2U5CGe3CXLGJPg");
-
+        appId ='OXJqhw8TXb4dgsQXw3uTo3IxYGdL1pj0qjV0g0vW';
+        restId = 'TKEZxU6JOZAsTwlpbPhAnJlstvn938KtuzNhFDCU';
         var currentUser = Parse.User.current();
 
         if (currentUser) {
@@ -17,5 +18,31 @@ myApp.controller('StudentsController',['$state',function($state){
           $state.go('home');
         }
 
+
+        students.initialize = function(){
+          students.newstudent = {};
+
+        }
+
+        students.addstudent = function(){
+          $http({
+                   method: 'POST',
+                   url: 'https://api.parse.com/1/classes/students',
+                   headers: {'X-Parse-Application-Id': appId, 'X-Parse-REST-API-Key': restId,
+                   'X-Parse-Revocable-Session': '1','Content-Type': 'application/json'},
+                   data: students.newstudent
+
+                }).success(function(data){
+                   console.log(data)
+                   $state.go('students');
+
+                }).error(function(data){
+                   console.log(data)
+                });
+
+            students.initialize();
+        }
+
+            students.initialize();
 
 }]);
--- src/app/index.run.js
+++ src/app/index.run.js
@@ -9,6 +9,9 @@
   function runBlock($log) {
 
     $log.debug('runBlock end');
+
+    // $http.defaults.headers.common['X-Parse-Application-Id'] = 'OXJqhw8TXb4dgsQXw3uTo3IxYGdL1pj0qjV0g0vW';
+    // $http.defaults.headers.common['X-Parse-REST-API-Key'] = 'TKEZxU6JOZAsTwlpbPhAnJlstvn938KtuzNhFDCU';
   }
 
 })();
